c8d823c22ec91a83090c4c29a0d7f0e6e0ec9701
johancollazosdeveloper <johan.collazos.developer@gmail.com>
2024-09-02 22:58:17 -0500

    app-marvel-heroes
    Implementacion inicial de login y registro frente a base de datos firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
   firebase: {
-    projectId: 'fireauth-test-4af2e',
-    appId: '1:845151277427:web:f21e584716aa32a4463506',
-    storageBucket: 'fireauth-test-4af2e.appspot.com',
-    apiKey: 'AIzaSyA-j_hTEEJy9N6BPoAEk9sIcn-bI6kH4ok',
-    authDomain: 'fireauth-test-4af2e.firebaseapp.com',
-    messagingSenderId: '845151277427',
+    apiKey: "AIzaSyBZ-vurZ5687k6Wu-L-acuOf6AoWpkkupI",
+    authDomain: "app-marvel-heroes.firebaseapp.com",
+    projectId: "app-marvel-heroes",
+    storageBucket: "app-marvel-heroes.appspot.com",
+    messagingSenderId: "187763991420",
+    appId: "1:187763991420:web:c4a86ded4702eb4235faf9",
+    measurementId: "G-DV1ZRZWRGP"
   },
 };
